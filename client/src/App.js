3a3339e2aab78d1c15fb34426e98e14a6e2bac30
sean <seanoh.dev@gamil.com>
2020-11-13 21:21:28 -0700

    completed signin/signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      timley
+      <SignIn />
     </div>
   );
 }
