f8beedd4a2dc6499d6a27873004f7f9e04154ae9
szferguson <steven@datpixel.net>
2020-11-17 20:35:13 +0000

    Add create post modal

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -7,10 +7,39 @@ import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { Button } from '@material-ui/core';
+import { PostCreator } from '../PostCreator/PostCreator.jsx';
 
 import './LandingStyles.css'
 
 const LandingPage = (props) => {
+
+    const [renderModalObj, setRenderModalObj] = useState({"tags": false, "login":false, "post":false});
+
+    function renderModal(content) {
+        return (
+            <div className="fixed z-10 inset-0 overflow-y-auto">
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <div className="fixed inset-0 transition-opacity">
+                        <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                        </div>
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+                        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                            {content}
+                        </div>
+                    </div>
+            </div>
+        )
+    }
+
+    function checkModalState() {
+        if (renderModalObj.post) {
+            return renderModal(<PostCreator onCancel = {cancelModal.bind("post")} />);
+        }
+    }
+
+    function cancelModal(arg) {
+        setRenderModalObj(prev => ({...prev, arg:false}));
+    }
 
 
     return (
@@ -24,6 +53,7 @@ const LandingPage = (props) => {
                     </div>
             </div>
 
+            {checkModalState()}
 
             <div className="flex">
 
@@ -88,6 +118,7 @@ const LandingPage = (props) => {
 
                         <div className="flex justify-center px-6">
                         <div
+                            onClick={() => setRenderModalObj(prev => ({...prev, "post":true}))}
                             className="button text-white text-2xl font-semibold mb-2 w-full text-center rounded cursor-pointer shadow-md"
                             style={{height: "3.2rem"}}>
                             <p style={{paddingTop: "0.18rem"}}>Create Post</p>
