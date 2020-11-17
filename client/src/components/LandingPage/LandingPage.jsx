8cda9bc0c9adb7e4c1e11657299a10bdadd1450f
szferguson <steven@datpixel.net>
2020-11-17 20:43:46 +0000

    Add other modal states

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -23,7 +23,7 @@ const LandingPage = (props) => {
                         <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
                         </div>
                         <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
-                        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle w-1/2" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                             {content}
                         </div>
                     </div>
@@ -34,6 +34,10 @@ const LandingPage = (props) => {
     function checkModalState() {
         if (renderModalObj.post) {
             return renderModal(<PostCreator onCancel = {cancelModal.bind("post")} />);
+        } else if (renderModalObj.tags) {
+            return renderModal(<PostCreator onCancel = {cancelModal.bind("tags")} />);
+        } else if (renderModalObj.login) {
+            return renderModal(<PostCreator onCancel = {cancelModal.bind("login")} />);
         }
     }
 
@@ -80,6 +84,7 @@ const LandingPage = (props) => {
 
                     <div className="flex justify-center px-6">
                         <div
+                        onClick={() => setRenderModalObj(prev => ({...prev, "tags":true}))}
                         className="button text-white text-2xl font-semibold mb-2 w-full text-center rounded cursor-pointer shadow-md"
                         style={{height: "3.2rem"}}>
                         <p style={{paddingTop: "0.18rem"}}>Tags</p>
