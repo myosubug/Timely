29b2f8b891e25a1d98bb2a5c5a2c81848fc2f24f
Gaby Gonzaga <gabrielle.gonzaga@vizworx.com>
2020-12-03 16:39:01 -0700

    Removed unused path

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,6 @@ function App() {
     <main>
       <Switch>
         <Route path="/" component={LandingPage} exact />
-        <Route path="/settings" component={UserOverviewEdit} />
         <Route path="/useroverview/:username">
           <OverviewRouter />
         </Route>
