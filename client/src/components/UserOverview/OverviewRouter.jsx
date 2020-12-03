c3c165e14f31932d516d685956dd2b128b59092a
Liors99 <47369500+Liors99@users.noreply.github.com>
2020-12-03 16:03:46 -0700

    Added 404 for non existing users

--- client/src/components/UserOverview/OverviewRouter.jsx
+++ client/src/components/UserOverview/OverviewRouter.jsx
@@ -1,30 +1,52 @@
 import React, { createRef, useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
-import { loggedInUser } from '../../AppConfig.js'
+import { loggedInUser, SERVER_ADDRESS } from '../../AppConfig.js'
 import { UserOverviewEdit } from './UserOverviewEdit.jsx';
 import { UserOverviewAdmin } from './UserOverviewAdmin.jsx';
 import { UserOverviewView } from './UserOverviewView.jsx';
+import axios from 'axios';
+import NotFound from '../NotFound/NotFound.jsx';
+import { CircularProgress } from '@material-ui/core';
+
+const load = <div>
+  <CircularProgress />
+</div>
 
 
 function OverviewRouter() {
 
   let { username } = useParams();
 
-  const renderOverview = () => {
-    if (loggedInUser.username === username) {
-      return (<UserOverviewEdit username={username} />);
-    }
-    else if (loggedInUser.isAdmin) {
-      return (<UserOverviewAdmin username={username} />);
-    }
-    else {
-      return (<UserOverviewView username={username} />);
-    }
-  };
+  const [ret, setRet] = useState(load);
+
+  useEffect(() => {
+
+    axios.get(SERVER_ADDRESS + "/users/findUser/" + username)
+      .then(({ data }) => {
+        if (data) {
+          if (loggedInUser.username === username) {
+            setRet(<UserOverviewEdit username={username} />);
+          }
+          else if (loggedInUser.isAdmin) {
+            setRet(<UserOverviewAdmin username={username} />);
+          }
+          else {
+            setRet(<UserOverviewView username={username} />);
+          }
+        }
+        else {
+          setRet(<NotFound />)
+        }
+
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+
 
   return (
     <div>
-      { renderOverview()}
+      { ret}
     </div>
   );
 
